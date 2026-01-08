134de09f0d57097d51d22bd187d582a707caf941
Abdullah Clement Abdul Shakur <abdulshakuraclement@yahoo.com>
2026-01-08 21:15:57 +0000

    feature: registered TenantFilter into securityconfig

--- src/main/java/cloud/norgha/multi_tenant_saas_starter_template/multitenancy/security/SecurityConfig.java
+++ src/main/java/cloud/norgha/multi_tenant_saas_starter_template/multitenancy/security/SecurityConfig.java
@@ -1,4 +1,28 @@
 package cloud.norgha.multi_tenant_saas_starter_template.multitenancy.security;
 
+
+import cloud.norgha.multi_tenant_saas_starter_template.multitenancy.tenant.TenantFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth ->
+                        auth.anyRequest().authenticated())
+                .addFilterBefore(new TenantFilter(),
+                        org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter.class
+                        )
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
 }
