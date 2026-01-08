7567fc10858760c58248c2fe9c7fe1a0ad5c475e
Abdullah Clement Abdul Shakur <abdulshakuraclement@yahoo.com>
2026-01-08 20:36:46 +0000

    added TenantContext

--- src/main/java/cloud/norgha/multi_tenant_saas_starter_template/multitenancy/tenant/TenantContext.java
+++ src/main/java/cloud/norgha/multi_tenant_saas_starter_template/multitenancy/tenant/TenantContext.java
@@ -1,4 +1,22 @@
 package cloud.norgha.multi_tenant_saas_starter_template.multitenancy.tenant;
 
-public class TenantContext {
+public final class TenantContext {
+
+    private static final ThreadLocal<String> CURRENT_TENANT = new ThreadLocal<>();
+
+    private TenantContext(){}
+
+    public static void setTenantId(String tenantId){
+        CURRENT_TENANT.set(tenantId);
+    }
+
+
+    public static String getTenantId(){
+        return CURRENT_TENANT.get();
+    }
+
+
+    public static void clear(){
+        CURRENT_TENANT.remove();
+    }
 }
