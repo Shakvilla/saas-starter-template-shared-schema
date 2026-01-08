5ae18663f4a4c701e80b0ff192ba648a2dbfd982
Abdullah Clement Abdul Shakur <abdulshakuraclement@yahoo.com>
2026-01-08 20:53:22 +0000

    feature: tenant filtering implemented

--- src/main/java/cloud/norgha/multi_tenant_saas_starter_template/multitenancy/tenant/TenantFilter.java
+++ src/main/java/cloud/norgha/multi_tenant_saas_starter_template/multitenancy/tenant/TenantFilter.java
@@ -1,4 +1,33 @@
 package cloud.norgha.multi_tenant_saas_starter_template.multitenancy.tenant;
 
-public class TenantFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class TenantFilter extends OncePerRequestFilter {
+
+    private final TenantResolver tenantResolver = new TenantResolver();
+
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException{
+
+
+        try{
+            String tenantId = tenantResolver.resolveTenant(request);
+            TenantContext.setTenantId(tenantId);
+            filterChain.doFilter(request, response);
+        }finally{
+            TenantContext.clear();
+        }
+
+    }
 }
