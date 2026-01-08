1a2cd5e5ba62ca834af80f74231dac0c0856c695
Abdullah Clement Abdul Shakur <abdulshakuraclement@yahoo.com>
2026-01-08 20:47:07 +0000

    feature: Skeletal TenantResolver implemented

--- src/main/java/cloud/norgha/multi_tenant_saas_starter_template/multitenancy/tenant/TenantResolver.java
+++ src/main/java/cloud/norgha/multi_tenant_saas_starter_template/multitenancy/tenant/TenantResolver.java
@@ -1,4 +1,20 @@
 package cloud.norgha.multi_tenant_saas_starter_template.multitenancy.tenant;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 public class TenantResolver {
+
+    public static final String TENANT_HEADER = "X-Tenant-ID";
+
+    public String resolveTenant(HttpServletRequest request){
+
+        String tenantId = request.getHeader(TENANT_HEADER);
+
+        if(tenantId == null || tenantId.isBlank()){
+            throw new TenantMissingException("Missing X-Tenant-ID header");
+        }
+
+
+        return tenantId;
+    }
 }
